5a70200d09af45b74494b145009172412256111d
Zhejun Zhang <zhangdmn@bc.edu>
2025-12-04 16:01:18 -0500

    Updated settings.py for render deployment

--- src/config/settings.py
+++ src/config/settings.py
@@ -189,7 +189,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (User uploads)
 MEDIA_URL = '/media/'
